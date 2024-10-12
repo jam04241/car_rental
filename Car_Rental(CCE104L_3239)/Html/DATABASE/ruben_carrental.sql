d400020bd0bf28386e98e6d1a1550ce5d16c30a6
Jam04241 <akog486@gmail.com>
2024-10-12 23:28:27 +0800

    update sql file

--- Car_Rental(CCE104L_3239)/Html/DATABASE/ruben_carrental.sql
+++ Car_Rental(CCE104L_3239)/Html/DATABASE/ruben_carrental.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 08, 2024 at 03:16 AM
+-- Generation Time: Oct 12, 2024 at 05:26 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,16 +24,36 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tbl_admin`
+--
+
+CREATE TABLE `tbl_admin` (
+  `admin_id` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_admin`
+--
+
+INSERT INTO `tbl_admin` (`admin_id`, `email`, `password`) VALUES
+(1, 'admin@gmail.com', '123');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbl_client_cust_serv`
 --
 
 CREATE TABLE `tbl_client_cust_serv` (
   `client_id` int(11) NOT NULL,
   `ID_ACC` int(11) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `Contact_no` int(13) NOT NULL,
+  `fullname` varchar(50) NOT NULL,
+  `Contact_no` bigint(20) NOT NULL,
   `addrss` varchar(50) NOT NULL,
-  `msgs` text NOT NULL
+  `msgs` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -46,17 +66,20 @@ CREATE TABLE `tbl_client_payment` (
   `reference_no` int(11) NOT NULL,
   `ID_ACC` int(11) NOT NULL,
   `rent_no` int(11) NOT NULL,
-  `Modeofpay` enum('e-pay','cash','debit_card') NOT NULL,
-  `dp` decimal(7,2) NOT NULL
+  `modeofpay` varchar(20) NOT NULL,
+  `dp` decimal(7,2) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tbl_client_payment`
 --
 
-INSERT INTO `tbl_client_payment` (`reference_no`, `ID_ACC`, `rent_no`, `Modeofpay`, `dp`) VALUES
-(1, 9, 7, '', 200.00),
-(2, 9, 7, '', 900.00);
+INSERT INTO `tbl_client_payment` (`reference_no`, `ID_ACC`, `rent_no`, `modeofpay`, `dp`, `created_at`) VALUES
+(1, 9, 1, 'debit-card', 456.00, '2024-10-12 07:35:41'),
+(2, 10, 2, 'debit-card', 780.00, '2024-10-12 13:56:15'),
+(3, 10, 3, 'cash', 123.00, '2024-10-12 14:07:00'),
+(4, 6, 4, 'cash', 87534.00, '2024-10-12 15:04:14');
 
 -- --------------------------------------------------------
 
@@ -69,20 +92,24 @@ CREATE TABLE `tbl_rent_car` (
   `ID_ACC` int(11) NOT NULL,
   `valid_id` varchar(20) NOT NULL,
   `license_no` varchar(20) NOT NULL,
+  `contact_no` bigint(20) NOT NULL,
+  `addrss` varchar(50) NOT NULL,
   `car_type` enum('SUV','Van','Sedan','Loaf') NOT NULL,
   `Vehicle` set('Honda','Nissan','Ford','Toyota') NOT NULL,
   `date_rental` date NOT NULL,
-  `date_period` date NOT NULL
+  `date_period` date NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tbl_rent_car`
 --
 
-INSERT INTO `tbl_rent_car` (`rent_no`, `ID_ACC`, `valid_id`, `license_no`, `car_type`, `Vehicle`, `date_rental`, `date_period`) VALUES
-(2, 6, 'gana123', 'gana456', 'Sedan', 'Honda', '2024-10-22', '2024-10-30'),
-(7, 9, 'tut324', 'yuyu133', 'SUV', 'Honda', '2024-10-05', '2024-10-13'),
-(8, 9, 'sam456', 'exap123', 'SUV', 'Toyota', '2024-10-13', '2024-10-30');
+INSERT INTO `tbl_rent_car` (`rent_no`, `ID_ACC`, `valid_id`, `license_no`, `contact_no`, `addrss`, `car_type`, `Vehicle`, `date_rental`, `date_period`, `created_at`) VALUES
+(1, 9, 'bvc213', 'fgh324', 98765, 'ghjg', 'Van', 'Nissan', '2024-11-09', '2024-11-11', '2024-10-12 07:35:35'),
+(2, 10, 'LO314', 'KO4324', 956732423, 'Matina, Davao City', 'Loaf', 'Nissan', '2024-10-31', '2024-11-09', '2024-10-12 13:56:07'),
+(3, 10, 'qwertyu', '3456u7i', 2345678, 'qewrty', 'Sedan', 'Toyota', '2024-11-09', '2024-11-16', '2024-10-12 14:06:53'),
+(4, 6, 'FOI76845634', 'GHO45365765', 98564643, 'Calinan ', 'Loaf', 'Toyota', '2024-10-19', '2024-11-09', '2024-10-12 15:04:05');
 
 -- --------------------------------------------------------
 
@@ -99,16 +126,19 @@ CREATE TABLE `tbl_support_details` (
   `valid_id2` varchar(11) NOT NULL,
   `affiliation` varchar(50) NOT NULL,
   `addrss` varchar(50) NOT NULL,
-  `contact_no` int(13) NOT NULL
+  `contact_no` bigint(20) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tbl_support_details`
 --
 
-INSERT INTO `tbl_support_details` (`sub_no`, `ID_ACC`, `rent_no`, `ref_name`, `valid_id1`, `valid_id2`, `affiliation`, `addrss`, `contact_no`) VALUES
-(1, 9, 7, 'Oskar Ciano', '0', '0', 'Sugar Dada', 'Matina, Davao City', 976523434),
-(2, 9, 7, 'Oskar Ciano', 'sam890', 'exa345', 'Father', 'Matina, Davao City', 2147483647);
+INSERT INTO `tbl_support_details` (`sub_no`, `ID_ACC`, `rent_no`, `ref_name`, `valid_id1`, `valid_id2`, `affiliation`, `addrss`, `contact_no`, `created_at`) VALUES
+(1, 9, 1, 'sanay', 'valid21423', 'valid875763', 'bngfgye', 'tuy', 987654, '2024-10-12 07:36:14'),
+(2, 10, 2, 'James Estorsos3', 'GJH6542', 'ASD14343', 'Ka grupo ', 'Mintal, Davao City', 965423444, '2024-10-12 13:56:40'),
+(3, 10, 2, 'hgjbm', 'asfgh342', 'mgnb324', 'asdfsgh', 'ewwrth', -98765, '2024-10-12 14:07:11'),
+(4, 6, 4, 'Gesef Mari', 'bb060404031', 'bbcakes143', 'Baby Cakes akong love story', 'Padre Pios, Bago Oshiro, Davao City', 95312456, '2024-10-12 15:05:20');
 
 -- --------------------------------------------------------
 
@@ -135,11 +165,20 @@ INSERT INTO `tbl_userlogin` (`ID_ACC`, `Fname`, `Lname`, `Gender`, `email`, `pas
 (6, 'sample', 'sample', 'Male', 'sample@gmail.com', '$2y$10$Q//wMTv/9Kxu5cUx9ld6meYhCxXBpv91e8qGXUzsCf2/jYU2kxhc6', 'sample'),
 (7, 'em', 'mail', 'Female', 'email@gmail.com', '$2y$10$E4IIVErLcQcHQxMAp9e6rOl6Ii6OlOOcT5A9s6scRHQAgS.iymVVm', 'password'),
 (8, 'Stephen ', 'Tatel', 'Male', 'stephen@umindanao.ph', '$2y$10$XTQ6UhaxnidlGrSVbBPYq.1tQvdLuNUswYPNgzu6IKQwggkBeLgbW', 'ugelovelife'),
-(9, 'Brendan', 'Ciano', 'Male', 'Ciano@umindanao.edu', '$2y$10$iInxm.UMe2d7asoigogzdeAtUxb71/0JnNF13Yz.BmF5chGLIqxRS', '12345');
+(9, 'Brendan', 'Ciano', 'Male', 'Ciano@umindanao.edu', '$2y$10$iInxm.UMe2d7asoigogzdeAtUxb71/0JnNF13Yz.BmF5chGLIqxRS', '12345'),
+(10, 'Rameses', 'Naquila', 'Female', 'Naquila@gmail.com', '$2y$10$snD5.YxVd/OxFkSU9m7uBeWamPx0MY/3AHhWKSYLpHSv5F1fwE2K6', '123456'),
+(11, 'Josh Andrei ', 'Magcalas', 'Male', 'josh@gmail.com', '$2y$10$f0WJU44mNWWq41qw7ljz9OrgGFgOc426QGT4xnYwUVDm7jL1amNka', '1234567890'),
+(12, 'Harold', 'Naquila', 'Male', 'Naquila@gmail.com', '$2y$10$9ivdVskTOkqyR1Z7P18NlOzRz4BngGvwWraS0vAwBM14EFPqkAyxK', '123456');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `tbl_admin`
+--
+ALTER TABLE `tbl_admin`
+  ADD PRIMARY KEY (`admin_id`);
 
 --
 -- Indexes for table `tbl_client_cust_serv`
@@ -176,6 +215,12 @@ ALTER TABLE `tbl_userlogin`
 --
 
 --
+-- AUTO_INCREMENT for table `tbl_admin`
+--
+ALTER TABLE `tbl_admin`
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `tbl_client_cust_serv`
 --
 ALTER TABLE `tbl_client_cust_serv`
@@ -185,25 +230,25 @@ ALTER TABLE `tbl_client_cust_serv`
 -- AUTO_INCREMENT for table `tbl_client_payment`
 --
 ALTER TABLE `tbl_client_payment`
-  MODIFY `reference_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `reference_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `tbl_rent_car`
 --
 ALTER TABLE `tbl_rent_car`
-  MODIFY `rent_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `rent_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `tbl_support_details`
 --
 ALTER TABLE `tbl_support_details`
-  MODIFY `sub_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `sub_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `tbl_userlogin`
 --
 ALTER TABLE `tbl_userlogin`
-  MODIFY `ID_ACC` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `ID_ACC` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
